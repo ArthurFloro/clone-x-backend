6b63f89c0687ee96af52071635e14d0f4cd0992e
ArthurFloro <arthur.floro2006@gmail.com>
2026-01-14 18:08:04 -0300

    feat: criando validacao de email e funcao para retornar url publica

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,14 +1,21 @@
 import { RequestHandler } from "express";
 import { signupSchema } from "../schemas/signup";
 import z from "zod";
+import { findUserByEmail } from "../services/user";
+import { error } from "node:console";
 
 export const signup: RequestHandler = async (req, res) => {
-  // validar os dados recebidos
   const safeData = signupSchema.safeParse(req.body);
   if (!safeData.success) {
     return res.json({ error: z.treeifyError(safeData.error) });
   }
+
   // verificar email
+  const hasEmail = await findUserByEmail(safeData.data.email);
+  if (hasEmail) {
+    return res.json({ error: "Email já cadastrado" });
+  }
+
   // verifica o slug
   // gera o hash da senha
   // cria o usuario
